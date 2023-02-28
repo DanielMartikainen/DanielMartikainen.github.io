35e32eac03b67ed3b8f2cd32082829c4b181de37
DanielMartikainen <125378757+DanielMartikainen@users.noreply.github.com>
2023-02-28 11:12:21 +0200

    demo7

--- demo7/kirjautuminen.js
+++ demo7/kirjautuminen.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 
 function onkoKirjautunut(){
     let kirjautunut = 'kylla';
-    if(kirjautunut === 'kylla'){
-        document.getElementById('tervetulo_teksti').textcontent += localStorage.getItem("nimi");
+    if(kirjautunut === 'kylla') {
+        document.getElementById("tervetulo_teksti").textcontent += localStorage.getItem("nimi");
     }
 }
 

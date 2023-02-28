b909c6db752db6c5c4db2ec1d744054a943afbfb
DanielMartikainen <125378757+DanielMartikainen@users.noreply.github.com>
2023-02-28 11:06:08 +0200

    demo7

---
+++ demo6/kirjautuminen.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautuminen);
+
+function onkoKirjautunut(){
+let kirjautunut = 'kylla';
+if(kirjautunut === 'kylla'){
+    document.getElementById('tervetulo_teksti').textcontent += ' Daniel!';
+}
+}
